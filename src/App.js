da22001f78570729d055e687d805ce45a2761a5c
Yohei Higashi <yoheihigashi.pg@gmail.com>
2020-05-14 15:11:56 +0900

    about useEffect

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 const App = () => {
+
+  const APP_ID = '62cd9b33';
+  const APP_KEY = '491358775bd46b1d16c869d01f83baf7';
+  const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
+
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    console.log('effect has been run');
+  }, []);
+
+
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <form className="search-form">
+        <input className="search-bar" />
+        <button className="search-button" type="submit">Search</button>
+      </form>
+      <h1 onClick={() => setCounter(counter + 1)}>{counter}</h1>
     </div>
   );
 }
